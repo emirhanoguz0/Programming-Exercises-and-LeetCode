89d8c99635df02aeede43e76225ba3917c1e0aa7
Emirhan Oğuz <160913617+emirhanoguz0@users.noreply.github.com>
2024-07-13 17:27:08 +0300

    Update Valid Parentheses.py

--- LeetCode/Valid Parentheses.py
+++ LeetCode/Valid Parentheses.py
@@ -1,17 +1,14 @@
 class Solution(object):
     def isValid(self, s):
-        r = False
-        list1 = ["(","{","["]
-        list2 = [")","}","]"]
+        a = []
+        for c in s:
+            if c in "{[(":
+                a.append(c)
+            else:
+                if not a or c == "}" and a[-1] != "{" or c == "]" and a[-1] != "[" or c == ")" and a[-1] != "(":
+                    return False
+                a.pop(-1)
+        return not a
 
-        l = list(s)
-        if c in l in list1:
-            i1 = list1.index()
-            if list2[i] in l:
-                i2 = list2.index()
-                if i1 < i2:
-                    r = True
-        return r
-
-s = [">£#$½{[]}"]
-Solution().isValid(s)
+s = "["
+print(Solution().isValid(s))
