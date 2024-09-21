4e0b5de850fa1af25216ebf0b535cddad559d36a
Emirhan Oğuz <160913617+emirhanoguz0@users.noreply.github.com>
2024-09-21 20:12:33 +0300

    Create Binary_Search.py

---
+++ LeetCode/Easy/Binary_Search.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        r = len(nums) - 1
+        mid = -1
+        while mid != (l + r)//2:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
