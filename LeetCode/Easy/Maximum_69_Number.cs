2c3b954878b8f6ba0212590c4449d6273b3f12f5
Emirhan Oğuz <160913617+emirhanoguz0@users.noreply.github.com>
2024-11-19 00:13:44 +0300

    Create Maximum_69_Number.cs

---
+++ LeetCode/Easy/Maximum_69_Number.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int Maximum69Number(int num)
+    {
+        string strnum = num.ToString();
+        int l = strnum.Length;
+        string a = "";
+        for (int i = 0; i < l; i++)
+            a += "9";
+        
+        int b = int.Parse(a) - num;
+        if (b == 0)
+            return num;
+        int ll = b.ToString().Length;
+        int x = 1;
+        for (int i = 0; i < (ll-1); i++)
+            x *= 10;
+        
+        return num + (3 * x);
+    }
+}
